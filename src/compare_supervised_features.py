542d32a2d85a422c138664ecab6d2dbd51a1819e
maxRicardo <xaradrim@gmail.com>
2014-06-11 16:24:56 -0400

    major changes to data struct. of the features_id, and re-wrote the functions implementations. Now just missing the implementation of the comparios between feature groups and between runs

--- src/compare_supervised_features.py
+++ src/compare_supervised_features.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 import argparse
-import parse_feature_id as pfi
+import utils
+import feature_group
 import os
 
 
@@ -11,12 +12,13 @@ import os
 def parse_arguments():
 	parser = argparse.ArgumentParser(description = " Script desing to compare state and distrubition of features from Qiime- Supervised Learning ",prog = "compare_supervised_features")
 
-	parser.add_argument("-i",help = "Input Feature Importance Scores. ", required = True , dest = "features_path" , type = str)
+	parser.add_argument("-i",help = "Input Feature Importance Scores [REQUIRED] . ", required = True , dest = "features_path" , type = str)
 	parser.add_argument("-n",help = "total number of features from file to read",default = 99999, dest = "features" , type = int)
 	parser.add_argument("--accuracy",help = "will stop reading features at this 'accuracy score'",dest = "accuracy" , type = str)
 	parser.add_argument("-o",help = "The directory to where the results will be written to. ", default = "CSF_output", dest = "output_path", type = str)
+	parser.add_argument("-f",help = "force override of the output files if they exist", action='store_true' , dest = "override")
 	opt = parser.parse_args()
-	print opt.output_path
+
 	return opt
 
 
@@ -31,11 +33,18 @@ def main():
 	try :
 		os.mkdir(opt.output_path)
 	except OSError:
-		raise OSError("File path already exist meaning you already used this path or you are running defualt again")
+		if opt.override and os.path.exists(opt.output_path):
 
-	feature_id = pfi.parse_features(opt)
-	#pfi.normal_stats_fit_plots(feature_id,opt.output_path)
-	pfi.group_summary(feature_id["GG"][:,1],opt.output_path)
+			os.rmdir(opt.output_path)
+			os.mkdir(opt.output_path)
+			
+		else:
+			raise OSError("File path already exist!")
+
+	feature_id = utils.parse_feature_importance_scores(opt)
+	#utils.normal_stats_fit_plots(feature_id,opt.output_path)
+	feature_id["GG"].toString()
+	print utils.normality_check(feature_id["GG"],opt.output_path)
 
 
 
