7bc42a777c55530f67a2c37e6a4aab55f112e770
maxRicardo <xaradrim@gmail.com>
2014-06-18 17:40:58 -0400

    Adding and testing new scripts as dev. soon minor changes to main code for more option capablities to manage and code/features re-usage.

--- scripts/feature_importance_comparison.py
+++ scripts/feature_importance_comparison.py
@@ -9,18 +9,39 @@ import argparse
 import os
 
 
-
+import daedalus.utils as du
 
 
 def parse_argument():
 	parser = argparse.ArgumentParser(description = "" , prog = "feature_importance_comparison")
 
-	parser.add_argument("-i", help = "",required = True,dest = "features_path",type = str)
-	parser.add_argument("-t",help = "",required = True,dest = "otu_table",type = str)
-	parser.add_argument("-o",help = "",required = True , dest = "output_path",type = str)
-	parser.add_argument("-a",help = "",default = 2 , dest = "accuracy",type = int)
-	parser.add_argument("-n",help = "",default = 99999, dest = "features",type = int)
+	parser.add_argument("-i", help = "features score file ",required = True,dest = "features_path",type = str)
+	parser.add_argument("-t",help = "otu table of the analysis",required = True,dest = "otu_table",type = str)
+	parser.add_argument("-o",help = "output folder for the analysis",required = True , dest = "output_path",type = str)
+	parser.add_argument("-a",help = "accuracy to stop the picking of sample ",default = 2 , dest = "accuracy",type = int)
+	parser.add_argument("-n",help = "number of features to parse from features file",default = 1, dest = "features",type = int)
 
 	opt = parser.parse_args()
 
 	return opt
+
+
+
+def main():
+
+	opt = parse_argument()
+	feature_groups = du.parse_feature_importance_scores(opt)
+
+	for group in feature_groups:
+		if not feature_groups[group].isEmpty():
+			doc = open(opt.output_path+"/importance_otus.txt","w")
+			for i in range(opt.features):
+				doc.write(feature_group[group].get_features())
+			doc.close()
+	pass
+
+
+
+if __name__ == "__main__":
+
+	main()
