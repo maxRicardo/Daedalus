71aefd04e5bed7acf9bbda3c28dfc04a4a5a0eb2
maxRicardo <xaradrim@gmail.com>
2014-06-20 00:00:07 -0400

    New Script debug un runtime still need to test for execution time. There have been some results that will be summarize for the next entry maybe written and lately with some graphs.

--- scripts/feature_importance_comparison.py
+++ scripts/feature_importance_comparison.py
@@ -7,7 +7,7 @@
 
 import argparse
 import os
-
+import shutil as sh
 
 import daedalus.utils as du
 
@@ -15,15 +15,16 @@ import daedalus.utils as du
 def parse_argument():
 	parser = argparse.ArgumentParser(description = "" , prog = "feature_importance_comparison")
 
-	parser.add_argument("-i", help = "features score file ",required = True,dest = "features_path",type = str)
+	parser.add_argument("-i",help = "features score file ",required = True,dest = "features_path",type = str)
 	parser.add_argument("-O",help = "otu table of the analysis",required = True,dest = "otu_table",type = str)
 	parser.add_argument("-o",help = "output folder for the analysis",required = True , dest = "working_path",type = str)
 	parser.add_argument("-m",help = "Metadata for running the supervised learning",required = True,dest = "map_path",type = str)
 	parser.add_argument("-a",help = "accuracy to stop the picking of sample ",default = 2 , dest = "accuracy",type = int)
 	parser.add_argument("-n",help = "number of features to parse from features file",default = 99999, dest = "features",type = int)
-	parser.add_argument("-d",help = "determine the number of feautres you want to remove from sample",default= 0,dest = "units",type = str)
-	parser.add_argument("-e",help = " Only will work if -d given too. Set to scaling instead of in one shot ", action = "store_true",dest = "scale")
+	parser.add_argument("-d",help = "determine the number of feautres you want to remove from sample",default= 0,dest = "units",type = int)
+	parser.add_argument("-e,--scale",help = "Only will work if -d given too. Set to scaling instead of in one shot ",dest = "scale", action = "store_true")
 	parser.add_argument("-c",help = "Name of the category the supervised run before",dest = "cat",type = str)
+	parser.add_argument("-f",help = "To force overriding the existing output directory path", dest = "override",action = "store_true")
 	opt = parser.parse_args()
 
 	return opt
@@ -49,7 +50,7 @@ def main():
 		os.mkdir(opt.working_path)
 
 	elif opt.override:
-		os.rmdir(opt.working_path)
+		sh.rmtree(opt.working_path)
 		os.mkdir(opt.working_path)
 	else:
 		raise OSError(" File already exist!")
@@ -64,7 +65,13 @@ def main():
 				non_scaling(
 					opt.working_path,
 					feature_groups[group],
-
+					group,
+					opt.units,
+					opt.accuracy,
+					opt.features,
+					opt.otu_table,
+					opt.map_path,
+					opt.cat
 					)
 	pass
 
@@ -84,7 +91,7 @@ def main():
 
 
 
-def non_scaling(working_path,feature_group,group_name,units,out_table,map_path,category):
+def non_scaling(working_path,feature_group,group_name,units,accuracy,features,out_table,map_path,category):
 	
 	otus_list = []
 	features = feature_group.get_features()
@@ -95,8 +102,8 @@ def non_scaling(working_path,feature_group,group_name,units,out_table,map_path,c
 
 	#create the file for filtering
 	doc = open(working_path+"/otu_list.txt","w")
-	for i in otu_list:
-		doc.write(str(i))
+	for i in otus_list:
+		doc.write(str(i)+"\n")
 	doc.close()
 
 	#filter the otu table
@@ -116,19 +123,21 @@ def non_scaling(working_path,feature_group,group_name,units,out_table,map_path,c
 		working_path+"filter_non_scale_"+str(units)+"_otu_table.biom",
 		map_path,
 		category,
-		working_path+"/supervise_results_"+str(units)+"units"
+		working_path+"/supervise_results_"+str(units)+"_units"
 		))
 
 	# parse and compare results
-	summ_command = "summarize_supervised_features.py -i {} -o {}"
+	summ_command = "summarize_supervise_features.py -i {} -o {}"
 
 	os.system(summ_command.format(
-		working_path+"/supervise_results_"+str(units)+"units/features_importance_scores.txt",
-		"summary_supervised_features_units"+str(units)+".txt"
+		working_path+"/supervise_results_"+str(units)+"_units/feature_importance_scores.txt",
+		working_path
 		))
 
 	new_feature_groups = du.parse_feature_importance_scores(
-		working_path+"/supervise_results_"+str(units)+"units/features_importance_scores.txt",
+		working_path+"/supervise_results_"+str(units)+"_units/feature_importance_scores.txt",
+		accuracy,
+		features
 		)
 
 	du.compare_feature_groups(feature_group,new_feature_groups[group_name])
