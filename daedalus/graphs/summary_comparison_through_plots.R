d5b4af5f9348435035e77d51a969a5b91b72aac0
maxRicardo <xaradrim@gmail.com>
2014-07-31 17:08:39 -0400

    Fixed error of path execution on Rscript

--- daedalus/graphs/summary_comparison_through_plots.R
+++ daedalus/graphs/summary_comparison_through_plots.R
@@ -1,10 +1,11 @@
 #!/usr/bin/Rscript --vanilla --default-packages=utils
 
 library(ggplot2)
+args<-commandArgs(TRUE)
+
 reference = paste(args[4],"daedalus/graphs/summary_graphsR.R",sep="/")
 source(reference)
 
-args<-commandArgs(TRUE)
 
 table <- read.delim(args[1])
 graph_boxplot_summary_comparion(table,args[2],args[3])
